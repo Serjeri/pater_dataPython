3787380005b3ead79772cbbd6b1e0c3d36b183f0
Сергей <hohachka@mail.ru>
2021-09-27 14:53:07 +0300

    Update pater_data.py

--- pater_data.py
+++ pater_data.py
@@ -1,7 +1,7 @@
 #Создать патерные данные о погоде
 import requests, json
 
-URL_TEMPLATE = "http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=8ab8dd8e10c2a1ae46c279684b638c3e"
+URL_TEMPLATE = "http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid="
 w = requests.get(URL_TEMPLATE)
 dict =w.json()
 s = dict["main"]
